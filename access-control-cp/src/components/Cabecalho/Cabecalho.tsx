e31f8083138ce7fa86e0a688845cec1274bb4de5
biribillidev <biribillijoaovitor@gmail.com>
2025-10-05 17:59:45 -0300

    feat: tentativa de fazer o nome e o email persistirem usando o parse

--- access-control-cp/src/components/Cabecalho/Cabecalho.tsx
+++ access-control-cp/src/components/Cabecalho/Cabecalho.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+
 export default function Cabecalho() {
-    return (
-        <header>
-            <h1>Cp Front</h1>
-        </header>
-    );
+  const [user, setUser] = useState<{ nome: string; email: string } | null>(
+    null
+  );
+
+  useEffect(() => {
+    const auth = localStorage.getItem("auth");
+    if (auth) {
+      try {
+        const parsed = JSON.parse(auth);
+        if (parsed.nome && parsed.email) {
+          setUser({ nome: parsed.nome, email: parsed.email });
+        }
+      } catch {
+        setUser(null);
+      }
+    } else {
+      setUser(null);
+    }
+  }, []);
+
+  return (
+    <>
+      <header> Cp Front</header>
+
+      {user && (
+        <p className="user-info">
+          {user.nome} • {user.email}
+        </p>
+      )}
+    </>
+  );
 }
