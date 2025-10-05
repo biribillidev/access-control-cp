b811730794bfe6ec6b111589190706521c71244b
biribillidev <biribillijoaovitor@gmail.com>
2025-10-05 17:09:48 -0300

    feat: import do tailwind + css inicail da página

--- access-control-cp/src/main.tsx
+++ access-control-cp/src/main.tsx
@@ -6,6 +6,7 @@ import Login from './routes/Login/index.tsx';
 import Error from './routes/Error/index.tsx';
 import Home from './routes/Home/index.tsx';
 import Cadastro from './routes/Cadastro/index.tsx';
+import "./globals.css"
 
 const router = createBrowserRouter([
   {path:"/",element:<App/>,errorElement:<Error/>,children:[
