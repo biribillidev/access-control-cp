ddde9a5d5254012a9cf3ce318dffa6208b39ce58
biribillidev <biribillijoaovitor@gmail.com>
2025-10-05 18:21:24 -0300

    feat: finalizando pagina home

--- access-control-cp/src/routes/Home/index.tsx
+++ access-control-cp/src/routes/Home/index.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <main>
-      <h1 className="titulo">Home</h1>
+    <main className="flex flex-col items-center justify-center h-screen p-4">
+      <h1  className="titulo">Login efetuado com sucesso!</h1>
+      <p>Fim do projeto!</p>
+      <p>(nome e email estão no canto superior esquerdo da tela)</p>
     </main>
+    
   );
 }
